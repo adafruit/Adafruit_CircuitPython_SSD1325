3eb10e31d0f3fd2a7d4802f4fb991cf7ec83a061
Melissa LeBlanc-Williams <melissa@adafruit.com>
2019-11-12 10:25:33 -0800

    Updated Simpletest, changed pins on Gamma Example for Feather

--- examples/ssd1325_gamma.py
+++ examples/ssd1325_gamma.py
@@ -7,10 +7,10 @@ import adafruit_ssd1325
 displayio.release_displays()
 
 # This pinout works on a Metro and may need to be altered for other boards.
-spi = busio.SPI(board.SCL, board.SDA)
-tft_cs = board.D9
-tft_dc = board.D8
-tft_reset = board.D7
+spi = board.SPI()
+oled_cs = board.D5
+oled_dc = board.D6
+tft_reset = board.D9
 
 display_bus = displayio.FourWire(spi, command=tft_dc, chip_select=tft_cs, reset=tft_reset,
                                  baudrate=1000000)
